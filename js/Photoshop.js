8b9dd887557029d214f00df4b4bc8ce1e7e166d6
Kayson Li <kayson.li@morningstar.com>
2014-06-26 16:51:28 +0800

    update photoshop

--- js/Photoshop.js
+++ js/Photoshop.js
@@ -16,6 +16,56 @@
 		};
 	}
 
+	function fillChars(pixels, w, h) {
+		var result = [];
+		var chars = ['W', '#', 'K', 'D', 'J', 'i', ',', '.', ''];
+		var min = Math.min.apply(null, pixels),
+			max = Math.max.apply(null, pixels),
+			range = max - min,
+			step = range / chars.length;
+		for (var i = 0; i < pixels.length; i++) {
+			var p = pixels[i];
+			switch (true) {
+				case (p >= (min + step * 0) && (p < min + step * 1)):
+					p = chars[0];
+					break;
+				case p >= min + step * 1 && p < min + step * 2:
+					p = chars[1];
+					break;
+				case p >= min + step * 2 && p < min + step * 3:
+					p = chars[2];
+					break;
+				case p >= min + step * 3 && p < min + step * 4:
+					p = chars[3];
+					break;
+				case p >= min + step * 4 && p < min + step * 5:
+					p = chars[4];
+					break;
+				case p >= min + step * 5 && p < min + step * 6:
+					p = chars[5];
+					break;
+				case p >= min + step * 6 && p < min + step * 7:
+					p = chars[6];
+					break;
+				case p >= min + step * 7 && p < min + step * 8:
+					p = chars[7];
+					break;
+				case p >= min + step * 8 && p < min + step * 9:
+					p = chars[8];
+					break;
+				default:
+					p = '.';
+					break;
+			}
+			result.push(p);
+			if(i % w == 0 && i > 0){
+				result.push('\n');
+			}
+		};
+		console.log(result);
+		return result.join('');
+	}
+
 	ps.ascii = function(imageData, width, height) {
 		var pixels = [];
 		var data = imageData.data;
@@ -30,7 +80,7 @@
 				y: ~~(i / width)
 			});
 		};
-		var chars = ['W', '#', 'K', 'D', 'J', 'i', ',', '.', ''];
+
 		var result = [];
 		var div = 10,
 			w = width / div,
@@ -44,14 +94,15 @@
 		var square = [];
 		for (var row = 0; row < h; row++) {
 			for (var col = 0; col < w; col++) {
-				var count = {}, max = 0,
+				var count = {},
+					max = 0,
 					target;
 				for (y = 0; y < div; y++) {
 					for (x = 0; x < div; x++) {
 						var cx = col * w + x,
 							cy = row * h + y;
 						var pixel = pixels[cy * width + cx];
-						if(pixel == undefined){
+						if (pixel == undefined) {
 							continue;
 						}
 						var value = pixel.gray;
@@ -59,12 +110,15 @@
 						count[value]++;
 						if (count[value] > max) {
 							max = count[value];
-							square.push(value);
+							target = value;
 						}
 					}
 				};
+				square.push(value);
 			}
 		}
-		console.log(square);
+		// console.log(square);
+		var str = fillChars(square, w);
+		return str;
 	}
 })(window);
