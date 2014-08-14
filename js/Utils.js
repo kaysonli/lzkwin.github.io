5fbc683685149895df122115e71262f1fafaa1f0
Kayson Li <kayson.li@morningstar.com>
2014-08-14 10:19:12 +0800

    update Util lib.

--- js/Utils.js
+++ js/Utils.js
@@ -50,5 +50,40 @@ var Utils = {
 			decimal--;
 		}
 		return digits.slice(0, decimalIndex + decimal + 1).join('');
+	},
+
+	rgbToHexFormat: function() {
+		if(arguments.length === 3) {
+			return '#' + arguments[0].toString(16) + arguments[1].toString(16) + arguments[2].toString(16);
+		}
+
+		if(arguments.length === 1 && typeof arguments[0] === 'string') {
+			var exp = arguments[0],
+				start = exp.indexOf('('),
+				rgb = exp.slice(start + 1, exp.length - 1);
+			var parts = rgb.split(',');
+			return arguments.callee.call(null, ~~parts[0], ~~parts[1], ~~parts[2]);
+		}
+		return '';
+	},
+
+	hexToRgbFormat: function(hex) {
+		if(typeof hex === 'number') {
+			hex = '#' + hex.toString(16);
+		}
+		var val = hex.slice(1);
+		if(val.length === 3) {
+			var parts = val.split('');
+			val = '' + parts[0] + parts[0] + parts[1] + parts[1] + parts[2] + parts[2];
+		}
+		var number = parseInt(val, 16);
+		var r = number >> 16,
+			g = (number & 0xffff) >> 8,
+			b = (number & 0xff);
+		// parts = val.split('');
+		// var r = parseInt(val.slice(0, 2), 16),
+		// 	g = parseInt(val.slice(2, 4), 16),
+		// 	b = parseInt(val.slice(4, 6), 16);
+		return Utils.format('rgb({0},{1},{2})', r, g, b);
 	}
 };
