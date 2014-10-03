3e017e51b800e2865b8f742289521236f4705218
kayson <lzkwin@qq.com>
2014-10-03 11:48:18 +0800

    JD interview test.

---
+++ studio/interview-test/js/main.js
@@ -0,0 +1,39 @@
+(function() {
+	$(function() {
+		initSlider();
+	});
+
+	function initSlider() {
+		var sliderImages = ['images/banner.png', 'images/banner.png', 'images/banner.png', 'images/banner.png'];
+		var slider = new Slider(sliderImages);
+		setInterval(function() {
+			slider.move('right');
+		}, 2000);
+		$(".slider-btn").click(function() {
+			var direction = $(this).hasClass('prev') ? 'left' : 'right';
+			slider.move(direction);
+		});
+	}
+
+	function Slider(imagesArray) {
+		this.imagesArray = imagesArray;
+		this.current = 0;
+	}
+
+	Slider.prototype.move = function(direction) {
+		var len = this.imagesArray.length,
+			me = this,
+			current = this.current;
+		if (direction === 'left') {
+			current = (current - 1 + len) % len;
+		} else if (direction === 'right') {
+			current = (current + 1) % len;
+		}
+		$("img.banner-img").fadeOut(200, function() {
+			me.current = current;
+			$("ul.slider-nav li").removeClass('on');
+			$("ul.slider-nav li:nth-child(" + (me.current + 1) + ")").addClass('on');
+			$("img.banner-img").attr('src', me.imagesArray[me.current]).fadeIn();
+		});
+	}
+})();
